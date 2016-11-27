acb4155e02ddc7aed49492919ed8f67cb8ef6fa1
jpcurrier <email@jackpaynecurrier.com>
2016-11-27 14:24:41 -0500

    clean up js

--- image-loader/image-loader.js
+++ image-loader/image-loader.js
@@ -1,14 +1,14 @@
 /*! Image Loader 1.0.0 | MIT *
  * https://github.com/jpcurrier/image-loader !*/
 ( function( $ ){
-	$.fn.imageLoader = function( options ){
+  $.fn.imageLoader = function( options ){
 
-		// default options
-		var settings = $.extend({
+    // default options
+    var settings = $.extend({
       complete: null,
       success: null,
       fail: null
-		}, options );
+    }, options );
 
     var load = [],
       count = 0,
@@ -19,25 +19,27 @@
       else
         load.push( $( this ).css( 'background-image' ).slice( 4, -1 ).replace( /"/g, '' ) );
     } );
+    function loadFail(){
+      failed.push( load[ this.loadPos ] );
+    }
+    function loadAlways(){
+      count++;
+      if( count === load.length ){
+        if( typeof settings.success === 'function' && !failed.length )
+          settings.success();
+        if( typeof settings.fail === 'function' && failed.length )
+          settings.fail( failed );
+        if( typeof settings.complete === 'function' )
+          settings.complete();
+      }
+    }
     for( var i = 0; i < load.length; i++ ){
       $.ajax({
         url: load[ i ],
         loadPos: i
       })
-        .fail( function(){
-          failed.push( load[ this.loadPos ] );
-        } )
-        .always( function(){
-          count++;
-          if( count === load.length ){
-						if( typeof settings.success === 'function' && !failed.length )
-              settings.success();
-            if( typeof settings.fail === 'function' && failed.length )
-              settings.fail( failed );
-            if( typeof settings.complete === 'function' )
-              settings.complete();
-          }
-        } );
+      .fail( loadFail )
+      .always( loadAlways );
     }
   };
 } )( jQuery );
